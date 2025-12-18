c8ab4ea706927554802e2877c3bf638dc8c8bc74
JUNAID SHAIKH <117749591+Jun12aid@users.noreply.github.com>
2025-12-18 12:54:31 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
     onSuccess: async () => {
       // 🔴 force refetch of /auth/me
       await queryClient.invalidateQueries(["me"]);
-      navigate("/dashboard");
+      
     },
   });
 
