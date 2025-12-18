23567e997fc4a8497710ec6bf4a5a1a0c5ad1256
JUNAID SHAIKH <117749591+Jun12aid@users.noreply.github.com>
2025-12-18 12:55:19 +0530

    Update ProtectedRoute.jsx

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,13 +1,26 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function ProtectedRoute({ children }) {
   const { user, isLoading } = useAuth();
+  const location = useLocation();
 
-  if (isLoading) return null;
+  if (isLoading) {
+    return null; // or loader
+  }
 
+  // ❌ not logged in → go to login
   if (!user) {
     return <Navigate to="/login" replace />;
+  }
+
+  // ✅ logged in & trying to access login/register → redirect
+  if (
+    user &&
+    (location.pathname === "/login" ||
+      location.pathname === "/register")
+  ) {
+    return <Navigate to="/dashboard" replace />;
   }
 
   return children;
